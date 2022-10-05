a3ede25546e50253f9bda07e2f6b44e251e6be05
Ali Muhammad Farooq <ali@Alis-iMac-Pro.local>
2022-10-05 19:28:30 +0500

    z-index fix

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,14 +2,14 @@ import { FC } from "react";
 
 const Landing: FC = () => {
   return (
-    <div className="-z-10 flex w-full max-w-screen-lg flex-col items-center justify-center py-20 px-6">
-      <h4 className=" text-center text-lg opacity-70 sm:text-2xl">
+    <div className=" flex w-full max-w-screen-lg flex-col items-center justify-center py-20 px-6">
+      <h4 className="-z-10 text-center text-lg opacity-70 sm:text-2xl">
         Hello 👋, My name is Sahil Saleem
       </h4>
-      <h1 className="mt-5 text-center text-4xl font-black leading-tight tracking-wide opacity-90 sm:mt-4 sm:text-7xl sm:leading-tight md:text-8xl md:leading-tight">
+      <h1 className="-z-10 mt-5 text-center text-4xl font-black leading-tight tracking-wide opacity-90 sm:mt-4 sm:text-7xl sm:leading-tight md:text-8xl md:leading-tight">
         I CREATE WORLD CLASS VIDEO SOLUTIONS.
       </h1>
-      <div className="mt-7 flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-7">
+      <div className=" mt-7 flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-7">
         <button className="h-12 w-44 rounded-md bg-gray-200 font-medium transition-colors duration-300 hover:bg-gray-300 sm:h-14">
           Contact me
         </button>
