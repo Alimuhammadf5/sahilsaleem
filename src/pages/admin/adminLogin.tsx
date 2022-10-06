bd44d9fa21e4fffcba12ebc2822ddb6d935cacb6
Ali Muhammad Farooq <ali@Alis-iMac-Pro.local>
2022-10-06 14:30:57 +0500

    admin login

--- src/pages/admin/adminLogin.tsx
+++ src/pages/admin/adminLogin.tsx
@@ -1,14 +1,23 @@
-import { FC, useEffect } from "react";
+import { FC, FormEvent, useEffect, useState } from "react";
 import AdminState from "../../utils/adminStatus";
 import { trpc } from "../../utils/trpc";
 
 const AdminLogin: FC<{ admin: boolean; setAdmin: any }> = (props) => {
   const { data, mutate } = trpc.useMutation("admin.login");
+  const [inputData, setInputData] = useState<{
+    email: string;
+    password: string;
+  }>({
+    email: "",
+    password: "",
+  });
 
-  const handleAdminLogin = async () => {
+  const handleAdminLogin = async (e: FormEvent) => {
+    e.preventDefault();
+
     await mutate({
-      username: "admin",
-      password: "admin123",
+      username: inputData.email,
+      password: inputData.password,
     });
   };
 
@@ -25,6 +34,49 @@ const AdminLogin: FC<{ admin: boolean; setAdmin: any }> = (props) => {
         className="w-full max-w-lg rounded-md border py-16 px-10"
       >
         <h1 className="text-center text-2xl font-semibold">Admin Login</h1>
+
+        <div className="mt-10 w-full">
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-gray-700 opacity-60"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            autoComplete="username"
+            value={inputData.email}
+            onChange={(e) =>
+              setInputData({ ...inputData, email: e.target.value })
+            }
+            id="username"
+            className="mt-1 w-full rounded-md border border-gray-300 px-6 py-3 duration-300 focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+        <div className="mt-4 w-full">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700 opacity-60"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name=""
+            value={inputData.password}
+            onChange={(e) =>
+              setInputData({ ...inputData, password: e.target.value })
+            }
+            autoComplete="password"
+            id="password"
+            className="mt-1 w-full rounded-md border border-gray-300 px-6 py-3 duration-300 focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+
+        <button className="mt-6 h-14 w-full rounded-md bg-blue-600 text-white">
+          Login
+        </button>
       </form>
     </div>
   );
