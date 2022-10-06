4ac88dff12ae4e58c03fff40714449b6e44ca48c
Ali Muhammad Farooq <ali@Alis-iMac-Pro.local>
2022-10-06 19:03:14 +0500

    transition fix

--- src/components/mailModel.tsx
+++ src/components/mailModel.tsx
@@ -1,12 +1,21 @@
 import { FC } from "react";
 import { useRecoilState } from "recoil";
 import { mailState } from "../recoil/mailState";
+import { motion } from "framer-motion";
 
 const MailModel: FC = (props) => {
   const [ismailState, setMailState] = useRecoilState(mailState);
 
   return (
-    <div
+    <motion.div
+      variants={{
+        open: { opacity: 1 },
+        closed: { opacity: 0 },
+      }}
+      initial="closed"
+      animate="open"
+      exit="closed"
+      transition={{ duration: 0.3 }}
       className="fixed top-0 left-0 right-0 bottom-0 m-auto flex flex-col items-center justify-center bg-white bg-opacity-50 text-black backdrop-blur-xl"
       style={{ zIndex: 60 }}
     >
@@ -22,7 +31,7 @@ const MailModel: FC = (props) => {
       >
         Close
       </button>
-    </div>
+    </motion.div>
   );
 };
 
